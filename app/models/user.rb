5720f862439f8f603a80e850ea2f6ecf3fff2422
Seb <sek@gobas.de>
2011-06-24 14:54:58 +0200

    user.create --> team for user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,14 @@ class User < ActiveRecord::Base
   has_many :teammemberships
   has_many :teams, :through => :teammemberships
 
+  after_create :create_team
 
   validates_presence_of :name
   validates_length_of :name, :in => 3..15
+
+
+  def create_team
+    self.teams.create(:name => self.name)
+  end
+
 end
