2c18aa203fbda465f378a836990359f472d5e33a
Seb <sek@gobas.de>
2011-07-01 09:40:41 +0200

    rescue me

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,14 +5,22 @@ class GamesController < ApplicationController
   def create
     @ladder = Ladder.find(params[:ladder_id])
     @game = @ladder.games.build(params[:game])
-    
-    @team1 = Team.find(@game.team1_id)
-    @team2 = Team.find(@game.team2_id)
-    @game.team1_name = @team1.name
-    @game.team2_name = @team2.name
 
-    @game.save
-    redirect_to ladder_path(@ladder)
+    begin
+      team1 = Team.find(@game.team1_id)
+      team2 = Team.find(@game.team2_id)
+      @game.team1_name = team1.name
+      @game.team2_name = team2.name
+    rescue ActiveRecord::RecordNotFound
+      #do something nice
+    end
+
+    if @game.save
+      redirect_to ladder_path(@ladder)
+    else
+      flash[:error] = "Das Spiel konnte nicht angelegt werden"
+      render :new
+    end
   end
 
   def new
