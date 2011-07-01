d623dacccca1f2225ae8654d3e622e0179f622d5
Seb <sek@gobas.de>
2011-07-01 16:03:21 +0200

    try to improve exceptions...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+#  rescue_from ActiveRecord::RecordNotFound, :with => :throw_404
+
   def render_forbidden
-    render :file => File.join(RAILS_ROOT, 'public/403.html'), :status => :forbidden, :layout => false   #work
+    render :file => File.join(Rails.root.to_s, 'public/403.html'), :status => :forbidden, :layout => false
+  end
+
+  def throw_404
+    render :file => File.join(Rails.root.to_s, 'public/404.html'), :status => :not_found, :layout => false
   end
 
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,24 +4,30 @@ class GamesController < ApplicationController
 
   def create
     @ladder = Ladder.find(params[:ladder_id])
-    @game = @ladder.games.build(params[:game])
+    if @ladder
+      unless params[:game].blank?       
+        @game = @ladder.games.build(params[:game])
+        
+        unless params[:game][:team1_id].blank? && params[:game][:team2_id].blank?
+          team1 = Team.find(@game.team1_id)
+          team2 = Team.find(@game.team2:id)
+          @game.team1_name = team1.name
+          @game.team2_name = team2.name
+        end
+        if @game.save
+          redirect_to ladder_path(@ladder)
+        else
+          flash[:error] = "Das Spiel konnte nicht angelegt werden"
+          render :new
+        end
+      end
 
-    begin
-      team1 = Team.find(@game.team1_id)
-      team2 = Team.find(@game.team2_id)
-      @game.team1_name = team1.name
-      @game.team2_name = team2.name
-    rescue ActiveRecord::RecordNotFound
-      #do something nice
-    end
-
-    if @game.save
-      redirect_to ladder_path(@ladder)
     else
       flash[:error] = "Das Spiel konnte nicht angelegt werden"
       render :new
     end
   end
+
 
   def new
     @ladder = Ladder.find(params[:ladder_id])
